6089b905394d2ecb468064f30dc0171ab689f9af
YuChun <bye22868@gmail.com>
2022-04-13 14:31:36 +0800

    商品功能 － Refator category：使用 Enum 類型

---
+++ src/main/java/com/judychen/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.judychen.springbootmall.constant;
+
+public enum ProductCategory {
+    CAR,
+    E_BOOK,
+    FOOD,
+}
--- src/main/java/com/judychen/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/judychen/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.judychen.springbootmall.rowmapper;
 
+import com.judychen.springbootmall.constant.ProductCategory;
 import com.judychen.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,7 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
